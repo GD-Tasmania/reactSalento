1f4831a79e3de1552595e35629ba267ef34bd4f2
Samuel Mejía <samuelmejiagir@gmail.com>
2022-11-18 13:01:29 -0500

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <nav>
+          <a>Inicio</a>
+          <a>Reserva</a>
+          <a>Contacto</a>
+        </nav>
       </header>
-    </div>
+      <section>
+        <h2 class="salento">Salento</h2>
+        <p class="texto1">Salento es una ciudad andina de Colombia, al oeste de Bogotá. Es conocida por sus fincas cafeteras y la vegetación de su entorno. Al este se encuentra el Valle del Cocora, donde las palmas de cera, un símbolo nacional, albergan loros poco comunes. La ciudad es una vía de acceso a las cimas nevadas del Parque nacional natural Los Nevados, al noreste.</p>
+        <img class="gente" src="myfirstproject\src\imagen\mirador-salento-quindio.jpg"></img>
+        <div class="clear"></div>
+        <h2 class="salento1">Problematicas en Salento</h2>
+        <img class="gente1" src="myfirstproject\src\imagen\atardecer.jpg"></img>
+        <p class="texto2" >El turismo, que puede ser para muchos municipios una alternativa de desarrollo, puede traer resultados negativos si no se sabe planificar. Este es el caso de Salento. El turismo en Salento no se planeó adecuadamente. Por eso, cuando llega la temporada alta pueden arribar hasta 10 mil viajeros que deben compartir espacio, agua y parqueaderos diseñados para los siete mil habitantes que residen allí.</p>
+        <img class="gente2" src="myfirstproject\src\imagen\saturacion.jpeg"></img>
+        <h2 class="salento2">"Hay límites turísticos que no se deben pasar en alto" </h2>
+        <p class="texto3">Los turistas para ingresar a zonas turísticas como Salento tardan entre dos o tres horas debido a la congestión vehicular, ya que no hay parqueaderos con grandes espacios para los miles de carros que ingresan.</p>
+        <p class="texto3">Entre sus mayores problemas esta que faltan zonas de parqueo, hay congestion vehicular, aglomeración de personas, vías pequeñas y problemas ambientales.</p>
+        <p class="texto4">El principal problema está en la planeación de la visita a Salento, ya que al ser un lugar tan turístico debería tener un límite de entrada de personas por medio de una planeación. "El turismo en el municipio de Salento no es agendado lo que genera mucho descontrol y saturación turística en temporadas altas."</p>
+      </section>
+    </main>
   );
 }
 
